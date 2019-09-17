e48e64f6b9ef9295235ad0177bac3b1cd2415474
Zack Bumpous <upwork.ztb@gmail.com>
2019-09-17 18:16:04 -0500

    working on array-methods assignment, I am seeing in my console that it has the data i am looking for

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -57,17 +57,41 @@ const runners = [
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
+
 let fullNames = [];
+  //needs for each so probably use for each, then going to need a push to the new array given to us
+  runners.forEach(runner => {
+    fullNames.push('${runner.first_name} ${runner.last_name}');
+  });
 console.log(fullNames);
+
+
+
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
+
 let firstNamesAllCaps = [];
+  //going to have to use the toUpperCase method at some point as well as .map
+  runners.map((runner, index) =>{
+    firstNamesAllCaps[index] = runner.first_name.toUpperCase();
+  })
 console.log(firstNamesAllCaps);
+
+
+
+
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+  runnersLargeSizeShirt = runners.filter(runner => {
+    if (runner.shirt_size === "L") {
+      return runner;
+    }
+  });
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
